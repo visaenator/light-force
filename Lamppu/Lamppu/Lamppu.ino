659f9c1270056f493f547591de6de4e96bacd808
Joonas Nikula <Joonas Nikula>
2016-05-14 16:00:15 +0300

    LCD madness

--- Lamppu/Lamppu/Lamppu.ino
+++ Lamppu/Lamppu/Lamppu.ino
@@ -3,39 +3,81 @@
  Created:	5/9/2016 1:16:41 PM
  Author:	Jodi
 */
-
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 String inputString = "";
 volatile boolean mutex;
 volatile boolean stringComplete = false;
+int ledPin1 = 5;
+int ledPin2 = 6;
+int ledPin3 = 7;
+int sleep = 2000;
+int currently = 7;
+int buttonState = 0;
 
 
 // the setup function runs once when you press reset or power the board
 void setup() {
 	Serial.begin(9600);
+	lcd.begin(16, 2);
 	inputString.reserve(256);
 	mutex = false; 
+	/*pinMode(ledPin1, OUTPUT);
+	pinMode(ledPin3, OUTPUT);
+	pinMode(ledPin2, INPUT);*/
+
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
+	//lcd.print("ASD");
+	/*analogWrite(currently, 255);
+	buttonState = digitalRead(ledPin2);
+	if(buttonState == HIGH){
+		changeLedState();
+	}*/
   if(!mutex && stringComplete){
+		lcd.clear();
 		Serial.println(inputString);
+		if(inputString.length() < 16){
+			lcd.print(inputString);
+		}
+		else if(inputString.length() > 16 && inputString.length() <= 32){
+			lcd.print(inputString.substring(0, 16));
+			lcd.setCursor(0,1);
+			lcd.print(inputString.substring(16, inputString.length()));
+		}
+		else{
+			lcd.print("FU TALK LESS");
+		}
 		inputString = "";
 		mutex = true;
 		stringComplete = false; 
 	}
 }
 
+void changeLedState(){
+		analogWrite(currently, 0);
+		if(currently == 5){
+				currently = 7;	
+			}
+			else{
+				currently = 5;
+			}
+}
+
 //Interrupt handler for UART
 void serialEvent(){
 	while(Serial.available()){
-		mutex = false; 
+		mutex = true; 
 		char inChar = (char)Serial.read();
 		inputString += inChar;
-		if(inChar == '\n'){
+
+		if(inputString.substring(inputString.length() - 2, inputString.length()).equals("CR")){
+			//Serial.println("ASD");
 			stringComplete = true;
-			mutex = true;
-			Serial.println(inputString);
+			mutex = false;
+			
 		}
 	}
 }
